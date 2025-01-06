40717f0cf2ac1a26c5d564025281137a718bb4fa
ivan.kos <ivan.koss@sombrainc.com>
2025-01-06 18:03:42 +0200

    base structure

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  productService: ProductService = inject(ProductService)
+  products: Product[] | undefined;
 
+  ngOnInit() {
+    this.productService.getProducts().subscribe((products: Product[]) => {
+      this.products = products;
+
+      console.log('this.products', this.products);
+    })
+  }
 }
